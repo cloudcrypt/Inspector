e851a1375f570be3a899d0b859ef5b8c8e72c1fa
Daniel D <cloudcrypt@users.noreply.github.com>
2017-10-23 15:18:01 -0600

    Added Simple Driver and TestClass, Started implementing Inspector.inspect

---
+++ src/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[] args) {
+        Inspector inspector = new Inspector();
+        inspector.inspect(new TestClass(), true);
+    }
+}
--- src/Inspector.java
+++ src/Inspector.java
@@ -1,2 +1,11 @@
+
 public class Inspector {
+
+    public Inspector() { }
+
+    public void inspect(Object obj, boolean recursive) {
+        System.out.printf("Inspecting object: %s\n", obj.toString());
+        
+    }
+
 }
