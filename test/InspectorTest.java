de24ace556ff73f09e7453c0089273a49659cd0a
Daniel D <cloudcrypt@users.noreply.github.com>
2017-10-24 02:16:51 -0600

    Added testClassA unit test

--- test/InspectorTest.java
+++ test/InspectorTest.java
@@ -1,9 +1,33 @@
+import TestDriver.*;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InspectorTest {
     @Test
-    public void testSomething() {
-        assertEquals(0, 0);
+    public void testClassA() {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outStream));
+        ClassA cls = new ClassA();
+        Inspector inspector = new Inspector();
+        inspector.inspect(cls, false);
+        String[] outputValues = new String[] {
+                "Class: TestDriver.ClassA",
+                "Immediate Superclass: java.lang.Object",
+                "Implemented Interfaces: \n" +
+                "\tjava.io.Serializable\n" +
+                "\tjava.lang.Runnable",
+                "Method: run",
+                "Method: toString",
+                "Method: setVal",
+                "Method: getVal",
+                "Method: printSomething"
+        };
+        Arrays.stream(outputValues).forEach(s -> assertTrue(outStream.toString().contains(s)));
     }
 }
