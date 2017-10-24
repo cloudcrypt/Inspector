e815ddb13fd215bd5a61ed892ea6aeff527b75a8
Daniel D <cloudcrypt@users.noreply.github.com>
2017-10-24 13:08:25 -0600

    Applied rename method refactoring to Inspector.print

--- src/Inspector.java
+++ src/Inspector.java
@@ -11,21 +11,21 @@ public class Inspector {
         System.out.printf("Class: %s\n", cls.getName());
         System.out.printf("Immediate Superclass: %s\n", cls.getSuperclass().getName());
         System.out.printf("Implemented Interfaces: \n");
-        print(cls.getInterfaces(), 1);
+        printNames(cls.getInterfaces(), 1);
         System.out.printf("Declared Methods: \n");
         Method[] methods = cls.getDeclaredMethods();
         if (methods.length > 0) {
             Arrays.stream(methods).forEach(m -> {
                 System.out.printf("\tMethod: %s\n", m.getName());
                 System.out.printf("\t\tExceptions Thrown: \n");
-                print(m.getExceptionTypes(), 3);
+                printNames(m.getExceptionTypes(), 3);
             });
         } else {
             System.out.println("\tNone\n");
         }
     }
 
-    private void print(Class[] list, int indentLevel) {
+    private void printNames(Class[] list, int indentLevel) {
         StringBuilder indentStringBuilder = new StringBuilder();
         for (int i = 0; i < indentLevel; i++) {
             indentStringBuilder.append('\t');
