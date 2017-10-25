d4c2b144f8850ca92fd7733bf65e15e77c058515
Daniel D <cloudcrypt@users.noreply.github.com>
2017-10-24 18:19:20 -0600

    Field indentLevel was created, and method getIndentStr was added to refactor out the repeated tab character strings

--- src/Inspector.java
+++ src/Inspector.java
@@ -3,38 +3,58 @@ import java.util.Arrays;
 
 public class Inspector {
 
+    private int indentLevel = 0;
+
     public Inspector() { }
 
     public void inspect(Object obj, boolean recursive) {
-        System.out.printf("Inspecting object: %s\n\n", obj.toString());
+        System.out.printf("%sInspecting object: %s\n\n", getIndentStr(), obj.toString());
         Class cls = obj.getClass();
-        System.out.printf("Class: %s\n", cls.getName());
-        System.out.printf("Immediate Superclass: %s\n", cls.getSuperclass().getName());
-        System.out.printf("Implemented Interfaces: \n");
-        printNames(cls.getInterfaces(), 1);
-        System.out.printf("Declared Methods: \n");
+        System.out.printf("%sClass: %s\n", getIndentStr(), cls.getName());
+        System.out.printf("%sImmediate Superclass: %s\n", getIndentStr(), cls.getSuperclass().getName());
+        System.out.printf("%sImplemented Interfaces: \n", getIndentStr());
+        indentLevel++;
+        printNames(cls.getInterfaces());
+        indentLevel--;
+        System.out.printf("%sDeclared Methods: \n", getIndentStr());
         Method[] methods = cls.getDeclaredMethods();
+        indentLevel++;
         if (methods.length > 0) {
             Arrays.stream(methods).forEach(m -> {
-                System.out.printf("\tMethod: %s\n", m.getName());
-                System.out.printf("\t\tExceptions Thrown: \n");
-                printNames(m.getExceptionTypes(), 3);
+                System.out.printf("%sMethod: %s\n", getIndentStr(), m.getName());
+                System.out.printf("%sExceptions Thrown: \n", getIndentStr(indentLevel+1));
+                indentLevel += 2;
+                printNames(m.getExceptionTypes());
+                indentLevel -= 2;
             });
         } else {
-            System.out.println("\tNone\n");
+            System.out.printf("%sNone\n", getIndentStr());
+        }
+        indentLevel--;
+    }
+
+    private void printNames(Class[] list) {
+        if (list.length > 0) {
+            Arrays.stream(list).forEach(c -> System.out.printf("%s%s\n", getIndentStr(), c.getName()));
+        } else {
+            System.out.printf("%sNone\n", getIndentStr());
         }
     }
 
-    private void printNames(Class[] list, int indentLevel) {
+    private String getIndentStr(int n) {
+        StringBuilder indentStringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            indentStringBuilder.append('\t');
+        }
+        return indentStringBuilder.toString();
+    }
+
+    private String getIndentStr() {
         StringBuilder indentStringBuilder = new StringBuilder();
         for (int i = 0; i < indentLevel; i++) {
             indentStringBuilder.append('\t');
         }
-        if (list.length > 0) {
-            Arrays.stream(list).forEach(c -> System.out.printf("%s%s\n", indentStringBuilder.toString(), c.getName()));
-        } else {
-            System.out.printf("%sNone\n", indentStringBuilder.toString());
-        }
+        return indentStringBuilder.toString();
     }
 
 }
