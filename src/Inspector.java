246376a821034b6979dc01743dde85e251263ec7
Daniel D <cloudcrypt@users.noreply.github.com>
2017-10-27 15:25:28 -0600

    Added javadocs

--- src/Inspector.java
+++ src/Inspector.java
@@ -130,6 +130,10 @@ public class Inspector {
         }
     }
 
+    /**
+     * Introspects and prints the details of a Constructor object
+     * @param c Constructor object to introspect
+     */
     private void printConstructor(Constructor c) {
         print("Constructor: %s", c.getName());
         printSpecificIndent("Modifiers:", indentLevel+1);
@@ -138,6 +142,10 @@ public class Inspector {
         indentAndExecute(2, () -> printNames(c.getParameterTypes()));
     }
 
+    /**
+     * Introspects and prints the details of a Method object
+     * @param m Method object to introspect
+     */
     private void printMethod(Method m) {
         print("Method: %s, Declared in %s", m.getName(), m.getDeclaringClass().getName());
         printSpecificIndent("Modifiers: ", indentLevel+1);
@@ -150,6 +158,10 @@ public class Inspector {
         indentAndExecute(2, () -> printNames(m.getExceptionTypes()));
     }
 
+    /**
+     * Introspects and prints the details of a Field object
+     * @param f Field object to introspect
+     */
     private void printField(Field f) {
         print("Field: %s, Declared in %s", f.getName(), f.getDeclaringClass().getName());
         printSpecificIndent("Modifiers:", indentLevel+1);
@@ -170,6 +182,10 @@ public class Inspector {
         } catch (IllegalAccessException e) { }
     }
 
+    /**
+     * Introspects and prints the value of an object
+     * @param value Object to introspect and print the value of
+     */
     private void printObjectValue(Object value) {
         if (value == null) {
             print("null");
@@ -196,6 +212,10 @@ public class Inspector {
         }
     }
 
+    /**
+     * Prints the type of a Class object, taking care of array types
+     * @param type Class object to print the type of
+     */
     private void printType(Class type) {
         if (!type.isArray()) {
             print(type.getName());
@@ -204,48 +224,101 @@ public class Inspector {
         }
     }
 
+    /**
+     * Checks if a Class object is of primitive or wrapped primitive types
+     * @param type Class object to check
+     * @return boolean value representing if class object is of primitive type
+     */
     private boolean isPrimitiveOrWrapper(Class<?> type) {
         return type.isPrimitive() || (type == Double.class || type == Float.class || type == Long.class ||
                 type == Integer.class || type == Short.class || type == Character.class ||
                 type == Byte.class || type == Boolean.class);
     }
 
+    /**
+     * Executes printType on a list of Class objects, if the
+     * list is not empty
+     * @param list List of Class objects to print
+     */
     private void printNames(Class[] list) {
         if (!isEmpty(list)) {
             Arrays.stream(list).forEach(this::printType);
         }
     }
 
+    /**
+     * Checks if any generic array type is empty
+     * @param list Array to check
+     * @param <T> Type parameter of arrays
+     * @return boolean value representing if array is empty
+     */
     private <T> boolean isEmpty(T[] list) {
         boolean empty = list.length == 0;
         if (empty) print("None");
         return empty;
     }
 
+    /**
+     * Checks if any generic ArrayList is empty
+     * @param list ArrayList to check
+     * @param <T> Type parameter of arrays
+     * @return boolean value representing if ArrayList is empty
+     */
     private <T> boolean isEmpty(ArrayList<T> list) {
         boolean empty = list.size() == 0;
         if (empty) print("None");
         return empty;
     }
 
+    /**
+     * Prints a string of tab characters, where the
+     * length of the indent string is defined by the field Inspector.indentLevel,
+     * and then prints supplied the format string with the given args
+     * @param format format string to print
+     * @param args arguments for printf
+     */
     private void print(String format, Object ... args) {
         System.out.print(getIndentStr());
         System.out.printf(format + "\n", args);
     }
 
+    /**
+     * Prints a string of tab characters, where the
+     * length of the indent string is defined by the parameter indentLevel,
+     * and then prints supplied the format string with the given args
+     * @param format format string to print
+     * @param indentLevel amount of tab characters to print
+     * @param args arguments for printf
+     */
     private void printSpecificIndent(String format, int indentLevel, Object ... args) {
         System.out.print(getIndentStr(indentLevel));
         System.out.printf(format + "\n", args);
     }
 
+    /**
+     * Generates an indent string of length defined by the field
+     * Inspector.indentLevel
+     * @return indent string
+     */
     private String getIndentStr() {
         return createIndentStr(indentLevel);
     }
 
+    /**
+     * Generates an indent string of specific length
+     * @param n length of indent string to generate
+     * @return indent string
+     */
     private String getIndentStr(int n) {
         return createIndentStr(n);
     }
 
+    /**
+     * Generates an indent string of specific length using
+     * StringBuilder
+     * @param n length of indent strings to generate
+     * @return indent string
+     */
     private String createIndentStr(int n) {
         StringBuilder indentStringBuilder = new StringBuilder();
         for (int i = 0; i < n; i++) {
@@ -254,6 +327,12 @@ public class Inspector {
         return indentStringBuilder.toString();
     }
 
+    /**
+     * Executes a lambda function after wrapping it between and increase
+     * and corresponding decrease of the current indentLevel
+     * @param indentAmount int amount to increase indentLevel to
+     * @param action lambda function of the action to perform at given indentation level
+     */
     private void indentAndExecute(int indentAmount, Runnable action) {
         indentLevel += indentAmount;
         action.run();
